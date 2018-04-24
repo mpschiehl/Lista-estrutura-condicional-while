954631facbf92e056ba83cbc5aa776750d1c7a8b
Marcio <mpschiehl@outlook.com>
2018-04-24 11:28:40 -0300

    desenvolvimento

--- ExercicioEstruturaWhile1.java
+++ ExercicioEstruturaWhile1.java
@@ -6,7 +6,7 @@ public class ExercicioEstruturaWhile1{
 
         nome = JOptionPane.showInputDialog(null, "Informe o Nome ou digite \"sair\" para encerrar.", "Exercicio1",JOptionPane.PLAIN_MESSAGE); 
 
-        while(!nome.equalsIgnoreCase("fim")){
+        while(!nome.equalsIgnoreCase("sair")){
             nome = JOptionPane.showInputDialog(null, "Informe o Nome ou digite \"sair\" para encerrar.", "Exercicio1",JOptionPane.PLAIN_MESSAGE);
 
         }
